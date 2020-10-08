f3dea66c9ae8c1a45701a5f88483e8d8885cfde7
Rafael Rosseto <rafael.rosseto@unesp.br>
2020-10-08 16:39:30 -0300

    feat: update themes and logo

--- mudatema.js
+++ mudatema.js
@@ -4,12 +4,12 @@ var temas = document.getElementById('temas');
 temas.addEventListener('change', function () {
   switch (this.value) {
     case "claro":
-      document.body.style.background = "#f1f7fa";//altera cor de fundo
-      document.querySelector('h1').style.color = "#072540" //altera cor do texto
+      document.body.style.background = "#efefef";//altera cor de fundo
+      document.querySelector('h1').style.color = "#31363b" //altera cor do texto
       break;
     case "escuro":
-      document.body.style.background = "#072540";//altera cor de fundo
-      document.querySelector('h1').style.color = "#9c4668" //altera cor do texto
+      document.body.style.background = "#31363b";//altera cor de fundo
+      document.querySelector('h1').style.color = "#efefef" //altera cor do texto
       break;
     default:
   }
